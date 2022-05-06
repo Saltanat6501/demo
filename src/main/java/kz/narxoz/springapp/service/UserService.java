1f344178a40261dc509e8517a01536dbfb3c2ef9
Saltanat6501 <103212482+Saltanat6501@users.noreply.github.com>
2022-05-06 11:26:24 +0600

    Add files via upload

---
+++ src/main/java/kz/narxoz/springapp/service/UserService.java
@@ -0,0 +1,32 @@
+package kz.narxoz.springapp.service;
+
+import kz.narxoz.springapp.model.Role;
+import kz.narxoz.springapp.model.User;
+import kz.narxoz.springapp.repository.RoleRepository;
+import kz.narxoz.springapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+import static java.util.List.*;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void save(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles(new HashSet<>(List.of(roleRepository.getById(1L)))); // USER
+        userRepository.save(user);
+    }
+}
